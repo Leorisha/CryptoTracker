98542b65a179e2ff7b7a80990d19f529625e6733
Ana Neto <a.neto@campus.fct.unl.pt>
2018-06-06 00:54:15 +0100

    Add alamofire.

--- CryptoTracker/Wallet.swift
+++ CryptoTracker/Wallet.swift
@@ -7,21 +7,36 @@
 //
 
 import Foundation
+import Alamofire
 
 class Wallet {
     
     static let shared = Wallet()
     
     var coins: [Coin] = []
+    var coinListDescription : String {
+        var listOfSymbols = ""
+        
+        for coin in coins {
+            listOfSymbols += coin.symbol
+            listOfSymbols += coin.symbol != coins.last?.symbol ? "," : ""
+        }
+        
+        return listOfSymbols
+    }
     
     private init() {
-        
+
         let symbols = ["BTC", "ETH", "LTC"]
-        
+
         for symbol in symbols {
             coins.append(Coin(with: symbol))
         }
     }
     
-    
+    func getPrices() {
+        
+        let url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=\(coinListDescription)&tsyms=USD"
+        Alamofire
+    }
 }
